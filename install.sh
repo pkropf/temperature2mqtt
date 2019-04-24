7a4ff16c1ca15d5837a8a1cef92e03aea8eb579a
Peter Kropf <peterk@ashbylumber.com>
2019-04-24 07:10:58 -0700

    added install.sh to deploy the files as needed and set up the service to run.

---
+++ install.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+
+cp temperature2mqtt.ini     /usr/local/bin/
+cp temperature2mqtt.py      /usr/local/bin/
+cp temperature2mqtt.sh      /usr/local/bin/
+cp temperature2mqtt.service /lib/systemd/system/
+
+systemctl daemon-reload
+systemctl enable temperature2mqtt
+systemctl start temperature2mqtt
