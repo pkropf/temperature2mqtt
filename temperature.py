bebde9301e3aa734aee65c8bba13fdadc4e881f8
Peter Kropf <pkropf@gmail.com>
2019-04-24 07:38:31 -0700

    cleanup

--- temperature.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python3.7
-
-import sys
-import Adafruit_DHT
-
-while True:
-    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
-    ftemperature = (temperature * (9/5)) + 32
-    print(f'Temp: {temperature} C, {ftemperature} f  Humidity: {humidity}')
