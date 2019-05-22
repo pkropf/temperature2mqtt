27eed45ba1cb52995a0efd05b0212c31d3eefb74
Peter Kropf <pkropf@gmail.com>
2019-05-22 12:22:41 -0700

    pulled the hardcoded dht type and port from the python code and added a section to the .ini file for them.

--- temperature2mqtt.py
+++ temperature2mqtt.py
@@ -15,6 +15,10 @@ config.read(ini_file)
 mqttc = mqtt.Client()
 broker = config.get('mqtt', 'host')
 broker_port = config.getint('mqtt', 'port')
+
+dht_type = config.getint('dht', 'type')
+dht_type = config.getint('dht', 'port')
+
 mqttc.connect(broker, broker_port)
 mqttc.loop_start()
 
@@ -24,11 +28,13 @@ keywords['nodename'] = platform.node()
 pause = config.getint('general', 'pause')
 sensor_topic = config.get('topics', 'sensor').format(**keywords)
 
+print(f"using configuration file {ini_file}")
 print(f"using mqtt broker {broker} on via port {broker_port}")
+print(f"using dht type {dht_type} on port {dht_port")
 print(f"publishing temperature to {sensor_topic}")
 
 while True:
-    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+    humidity, temperature = Adafruit_DHT.read_retry(dht_type, dht_port)
 
     sensor_data = f'{{"temperature": "{temperature}", "humidity": "{humidity}"}}'
     mqttc.publish(sensor_topic, sensor_data)
