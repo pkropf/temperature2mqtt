65d8861eb88cd9d2fd4776896e9049205ba0f4b2
Peter Kropf <pkropf@gmail.com>
2019-05-22 12:35:05 -0700

    added config option to specify the type of units for the temperature, celsius or fahrenheit

--- temperature2mqtt.py
+++ temperature2mqtt.py
@@ -17,7 +17,7 @@ broker = config.get('mqtt', 'host')
 broker_port = config.getint('mqtt', 'port')
 
 dht_type = config.getint('dht', 'type')
-dht_type = config.getint('dht', 'port')
+dht_port = config.getint('dht', 'port')
 
 mqttc.connect(broker, broker_port)
 mqttc.loop_start()
@@ -26,20 +26,23 @@ keywords = os.environ.copy()
 keywords['nodename'] = platform.node()
 
 pause = config.getint('general', 'pause')
+units = config.get('general', 'units')
 sensor_topic = config.get('topics', 'sensor').format(**keywords)
 
 print(f"using configuration file {ini_file}")
 print(f"using mqtt broker {broker} on via port {broker_port}")
-print(f"using dht type {dht_type} on port {dht_port")
-print(f"publishing temperature to {sensor_topic}")
+print(f"using dht type {dht_type} on port {dht_port}")
+print(f"publishing temperature to {sensor_topic} as {units}")
 
 while True:
     humidity, temperature = Adafruit_DHT.read_retry(dht_type, dht_port)
 
-    sensor_data = f'{{"temperature": "{temperature}", "humidity": "{humidity}"}}'
+    if units in ['f', 'F']:
+        temperature = (temperature * (9/5)) + 32
+
+    sensor_data = f'{{"temperature": "{temperature}", "units": "{units}", "humidity": "{humidity}"}}'
     mqttc.publish(sensor_topic, sensor_data)
 
-    ftemperature = (temperature * (9/5)) + 32
     print(sensor_data)
 
     time.sleep(pause)
