57a30a2ab4ed8c035c5fe7efbd8b1e9c8163140a
Peter Kropf <pkropf@gmail.com>
2019-07-11 11:37:57 -0700

    updated config file names

--- temperature2mqtt.py
+++ temperature2mqtt.py
@@ -9,39 +9,39 @@ import os
 from configHelper import findConfig
 
 
+keywords = os.environ.copy()
+keywords['nodename'] = platform.node()
+
 ini_file = findConfig('temperature2mqtt.ini')
 config = configparser.ConfigParser()
 config.read(ini_file)
 
-broker = config.get('mqtt', 'host')
-broker_port = config.getint('mqtt', 'port')
+broker = config.get('mqtt', 'host').format(**keywords)
+broker_port = int(config.get('mqtt', 'port').format(**keywords))
 
-dht_type = config.getint('dht', 'type')
-dht_port = config.getint('dht', 'port')
+dht_type = int(config.get('dht', 'type').format(**keywords))
+dht_pin = int(config.get('dht', 'pin').format(**keywords))
 
 # test if the broker has been configured, MQTT_BROKER is the default value in the
 # config file as distributed from the repository
-if broker == 'MQTT_BROKER':
+if broker == 'mqtt host':
     raise ValueError('broker not configured')
 
 mqttc = mqtt.Client()
 mqttc.connect(broker, broker_port)
 mqttc.loop_start()
 
-keywords = os.environ.copy()
-keywords['nodename'] = platform.node()
-
-pause = config.getint('general', 'pause')
-units = config.get('general', 'units')
+pause = int(config.get('general', 'pause').format(**keywords))
+units = config.get('general', 'units').format(**keywords)
 sensor_topic = config.get('topics', 'sensor').format(**keywords)
 
 print(f"using configuration file {ini_file}")
 print(f"using mqtt broker {broker} on via port {broker_port}")
-print(f"using dht type {dht_type} on port {dht_port}")
+print(f"using dht type {dht_type} on pin {dht_pin}")
 print(f"publishing temperature to {sensor_topic} as {units}")
 
 while True:
-    humidity, temperature = Adafruit_DHT.read_retry(dht_type, dht_port)
+    humidity, temperature = Adafruit_DHT.read_retry(dht_type, dht_pin)
 
     if units in ['f', 'F']:
         temperature = (temperature * (9/5)) + 32
